ea11c3c1d8ff27e4623cc9d8501d93d3c3a0038a
Oleg Grigoryev <oleg_grigoryev@epam.com>
2019-04-07 14:40:52 +0300

    lab5. Binding

---
+++ lab5/lab5/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace lab5
+{
+    public class Country
+    {
+        public string ImageFileName { get; set; }
+        [DisplayName("Name"), Category("Overall")]
+        public string Name { get; set; }
+        [DisplayName("Capital"), Category("Overall")]
+        public string Capital { get; set; }
+        [DisplayName("Population"), Category("Demography")]
+        [Description("Population, in bln.people")]
+        public double Population { get; set; }
+        [DisplayName("Size"), Category("Overall")]
+        [Description("Square in thousands sq. km.")]
+        public double Size{ get; set; }
+    }
+}
